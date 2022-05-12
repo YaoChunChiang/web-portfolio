a77214b8df741eea72d2ab57ed3f13c160db6660
Alex <t159753g@gmail.com>
2022-05-12 11:25:23 +0800

    add basename

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function Header () {
 function App() {
 
     return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
             <Header />
             <Routes>
